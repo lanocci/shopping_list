dacfc339752599df3f4fa962dd5c84cdcd87defc
lanocci <16540010+lanocci@users.noreply.github.com>
2021-03-27 16:44:34 +0900

    add login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(ShoppingListApp());
@@ -33,49 +32,56 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: 'email address'),
-                onChanged: (String value) {
-                  setState(() {
-                    email = value;
-                  })
+        body: Center(
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: 'email address'),
+              onChanged: (String value) {
+                setState(() {
+                  email = value;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'password'),
+              obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  password = value;
+                });
+              },
+            ),
+            Container(padding: EdgeInsets.all(8), child: Text(infoText)),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                child: Text('ユーザ登録'),
+                onPressed: () async {
+                  try {
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    await auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    await Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) {
+                        return ShoppingListPage();
+                      }),
+                    );
+                  } catch (e) {
+                    setState(() {
+                      infoText = "登録に失敗しました: ${e.toString()}";
+                    });
+                  }
                 },
               ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'password'),
-                obscureText: true,
-                onChanged: (String value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-              ),
-              Container(
-                padding: EdgeInsets.all(8),
-                child: Text(infoText)
-              ),
-              Container(
-                width: double.infinity,
-                child: ElevatedButton(
-                  child: Text('ユーザ登録'),
-                  onPressed: () async {
-                    try {
-                      final FirebaseAuth auth = FirebaseAuth.instance;
-                    }
-                  },
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
-      )
-    )
+      ),
+    ));
   }
 }
 
